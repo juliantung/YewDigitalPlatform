6b125019a2087d97cf1c753ad8a5a17f492acc9a
juliantung <156071429+juliantung@users.noreply.github.com>
2024-06-21 23:23:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    // Form animation
+    $(".form").on('click', function(){
+        $(this).addClass('active');
+    });
+
+    $(".submit").on('click', function() {
+        $(this).parent().parent().hide(300);
+        $(".ok_message").addClass("active");
+        pageTitle.textContent = "Feedback received"
+        $('#testing').show();
+    });
+/** 
+    $(".ok_message").on('click', function() {
+        $(this).removeClass("active");
+        $(".form").removeClass("active").show();
+    });
+**/
+    // Star rating functionality
+    let rating = 0;
+
+    window.highlightStars = function(num) {
+        for (let i = 1; i <= num; i++) {
+            $(`#ratings .star:nth-child(${i})`).addClass('highlighted');
+        }
+    };
+
+    window.resetStars = function() {
+        $('#ratings .star').removeClass('highlighted');
+    };
+
+    window.rate = function(num) {
+        rating = num;
+        resetStars();
+        highlightStars(num);
+        var pageTitle = document.getElementById('pageTitle');
+        if (num <= 3) {
+            pageTitle.textContent = "Tell us more";
+            $('#ratings').hide();
+            $('#testing').hide();
+            $('#feedback').show().addClass('active');
+        } else {
+            window.open('https://g.page/r/CR4Koahp2uHYEBM/review');
+            //Paste review link above
+            window.location.href = 'https://yewdigitallock.com.sg/digital-locks/digital-door-locks/';
+            //Paste navigator link above such as website
+            //Change for specific companies
+        }
+    };
+}); 
+
+
+
+
+
+
+
+
+
+
